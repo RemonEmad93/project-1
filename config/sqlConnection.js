6b1f4dc53e1a523f0ba49e400c3c8809108cb61f
Remon Emad <68864945+RemonEmad93@users.noreply.github.com>
2021-09-27 16:28:20 +0200

    clean sqlconnection ,remove unwanted bootstrap

--- config/sqlConnection.js
+++ config/sqlConnection.js
@@ -1,10 +1,7 @@
 var sql= require('mssql/msnodesqlv8');
 
 var config={
-
     connectionString: 'Driver={SQL Server Native Client 10.0};Server={.};Database={GNP};Trusted_Connection={yes}',
-    
-
 };
 
 var connection= sql.connect(config,(error)=>{
